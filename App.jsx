72e19714b4073bd21b282036f71fee30b587dcdc
Ajay Singh Yadav <ajayyadav442408@gmail.com>
2025-05-04 00:01:30 +0530

    add Welcome Screen

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
+import WelcomeScreen from './src/screens/WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
