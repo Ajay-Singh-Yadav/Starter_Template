40d389b1340c4763a65f79beb3d351da6e1a5db0
Ajay Singh Yadav <ajayyadav442408@gmail.com>
2025-05-03 23:50:51 +0530

    linear gradient

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,18 +2,26 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
+import LinearGradient from 'react-native-linear-gradient';
 const HomeScreen = () => {
   return (
-    <View>
-      <FontAwesome name="search" size={30} color="black" />;
+    <LinearGradient
+      colors={['#4c669f', '#3b5998', '#192f6a']}
+      style={styles.container}>
+      <FontAwesome name="search" size={30} color="black" />
       <Text style={styles.text}>HomeScreen</Text>
-    </View>
+    </LinearGradient>
   );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   text: {
     fontFamily: 'TenorSans-Regular',
     fontSize: 30,
